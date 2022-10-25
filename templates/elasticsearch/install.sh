1bec5ce66949c5f08fb9e216b5c23a7b8788c976
ROBIN Gilles <gilles.robin@uca.fr>
2022-10-25 11:25:31 +0200

    modif template elasticsearch

--- templates/elasticsearch/install.sh
+++ templates/elasticsearch/install.sh
@@ -1,8 +1,8 @@
 function install() {
 	echo "Installation des containers elasticsearch/kibana..."
 	sed $'/### containers ###/{e cat templates/elasticsearch/docker-compose.override.yml\n}' public/docker-compose.override.yml -i
+	sed $'/### volumes ###/{e cat templates/elasticsearch/volumes.yml\n}' public/docker-compose.override.yml -i
 	sed $'/### containers ###/{e cat templates/elasticsearch/docker-compose.yml\n}' public/docker-compose.yml -i
-	# sed $'/### volumes ###/{e cat templates/elasticsearch/volumes.yml\n}' public/docker-compose.yml -i
 	echo "Les docker-compose et docker-compose.override ont été mis à jour."
 	cat templates/elasticsearch/elasticsearch.env >> public/.env
 	echo "Le fichier .env a été modifié."
@@ -11,6 +11,7 @@ function install() {
 function unInstall() {
 	echo "désinstallation des containers elasticsearch/kibana..."
 	sed '/###> elasticsearch ###/,/### elasticsearch ###/d' public/docker-compose.override.yml -i
+	sed '/###> volumes_elasticsearch ###/,/### elasticsearch ###/d' public/docker-compose.override.yml -i
 	sed '/###> elasticsearch ###/,/### elasticsearch ###/d' public/docker-compose.yml -i
 	echo "les containers elasticsearch/kibana ont été désinstallés."
 }
