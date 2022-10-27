cdc79ae74fcbcea7b8bcf8696e485cb08e569c2d
ROBIN Gilles <gilles.robin@uca.fr>
2022-10-27 10:32:21 +0200

    fix erreur tag de fin elasticsearch pour suppression

--- templates/elasticsearch/install.sh
+++ templates/elasticsearch/install.sh
@@ -11,7 +11,7 @@ function install() {
 function unInstall() {
 	echo "désinstallation des containers elasticsearch/kibana..."
 	sed '/###> elasticsearch ###/,/### elasticsearch ###/d' public/docker-compose.override.yml -i
-	sed '/###> volumes_elasticsearch ###/,/### elasticsearch ###/d' public/docker-compose.override.yml -i
+	sed '/###> volumes_elasticsearch ###/,/### volumes_elasticsearch ###/d' public/docker-compose.override.yml -i
 	sed '/###> elasticsearch ###/,/### elasticsearch ###/d' public/docker-compose.yml -i
 	echo "les containers elasticsearch/kibana ont été désinstallés."
 }
